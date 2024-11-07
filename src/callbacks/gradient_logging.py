de0143b372db867b4f635c71ac77f3e21c4f3ffb
Artem Makoian <amakoian@constructor.university>
2024-11-07 12:27:03 +0000

    no grad clip by default

--- src/callbacks/gradient_logging.py
+++ src/callbacks/gradient_logging.py
@@ -1,15 +1,11 @@
+from typing import Optional
+
 import lightning as pl
 import torch
 
 
 class GradientLoggingCallback(pl.Callback):
-    def __init__(self, grad_clip_val = 1.):
-        self.grad_clip_val = grad_clip_val
-
     def on_after_backward(self, trainer, pl_module):
-        # Manually log the gradient norm
-        torch.nn.utils.clip_grad_norm_(pl_module.parameters(), self.grad_clip_val)
-
         grad_norms = [p.grad.norm(2).item() for p in pl_module.parameters() if p.grad is not None]
         mean_grad_norm = sum(grad_norms) / len(grad_norms) if grad_norms else 0
 
