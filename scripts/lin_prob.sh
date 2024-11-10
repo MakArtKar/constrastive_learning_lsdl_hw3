01c04b0402803660ba885d39b2290794ca05001d
Artem Makoian <amakoian@constructor.university>
2024-11-10 19:36:50 +0000

    fixed scripts + t-SNE in notebook

--- scripts/lin_prob.sh
+++ scripts/lin_prob.sh
@@ -1,4 +1,4 @@
-python -m src.train experiment=lin_prob model=stl10 data=stl10 num_classes=10 pretrained_experiment=sim_clr_gpu_transforms trainer=gpu logger=wandb logger.wandb.name=lin_prob_sim_clr_stl10
-python -m src.train experiment=lin_prob model=stl10 data=cifar10 num_classes=9 pretrained_experiment=sim_clr_gpu_transforms trainer=gpu logger=wandb logger.wandb.name=lin_prob_sim_clr_cifar10
-python -m src.train experiment=lin_prob model=stl10 data=stl10 num_classes=10 pretrained_experiment=byol trainer=gpu logger=wandb logger.wandb.name=lin_prob_byol_stl10
-python -m src.train experiment=lin_prob model=stl10 data=cifar10 num_classes=9 pretrained_experiment=byol trainer=gpu logger=wandb logger.wandb.name=lin_prob_byol_cifar10
+python -m src.train experiment=lin_prob model=stl10 data=stl10 num_classes=10 pretrained_experiment=sim_clr_gpu_transforms trainer=gpu logger=wandb logger.wandb.name=lin_prob_sim_clr_stl10 task_name=report paths.dir_to_save=lin_prob_sim_clr_stl10
+python -m src.train experiment=lin_prob model=stl10 data=cifar10 num_classes=9 pretrained_experiment=sim_clr_gpu_transforms trainer=gpu logger=wandb logger.wandb.name=lin_prob_sim_clr_cifar10 task_name=report paths.dir_to_save=lin_prob_sim_clr_cifar10
+python -m src.train experiment=lin_prob model=stl10 data=stl10 num_classes=10 pretrained_experiment=byol trainer=gpu logger=wandb logger.wandb.name=lin_prob_byol_stl10 task_name=report paths.dir_to_save=lin_prob_byol_stl10
+python -m src.train experiment=lin_prob model=stl10 data=cifar10 num_classes=9 pretrained_experiment=byol trainer=gpu logger=wandb logger.wandb.name=lin_prob_byol_cifar10 task_name=report paths.dir_to_save=lin_prob_byol_cifar10
